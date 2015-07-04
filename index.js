875d4c3ee470090066fc4b3b7302a635dc84065d
Yoshua Wuyts <i@yoshuawuyts.com>
2015-07-04 17:09:16 +0200

    format: use standard & remove dead deps

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const assert  = require('assert')
 const Promise = require('native-or-bluebird')
+const assert = require('assert')
 
 module.exports = each
 
-// Apply a function to all values.
-// @param {Mixed|Mixed[]} val
-// @param {Mixed} ctx
-// @return {Function}
-function each(fn, ctx) {
+// apply a function to all values
+// (fn, obj?) -> [any]|any
+function each (fn, ctx) {
   assert.equal(typeof fn, 'function')
-  return function(val) {
+  return function (val) {
     val = Array.isArray(val) ? val : [val]
     return Promise.resolve(val.forEach(fn, ctx))
   }
